8962fc9d3f8e3bc2ab7acdfe92488c21d99f786b
Dr Yann Golanski <github@kierun.org>
2023-04-08 19:20:58 +0100

    feat(NPC): skill names and personalities

--- pynpc/npc.py
+++ pynpc/npc.py
@@ -1,27 +1,64 @@
 # -*- coding: utf-8 -*-
 """NPC class."""
 from collections import namedtuple
+from pathlib import Path, PosixPath
 from secrets import choice
+from typing import cast
 
 import structlog
 
-from pynpc.skills import PROFESSIONS, get_skill_value
+from pynpc.skills import get_skill_value
 
 rlog = structlog.get_logger("pynpc.npoc")
 
 Skill = namedtuple("Skill", ["name", "rank"])
 
+# We know they are bullshit. However, they make a good starting
+# ponit to give some NPC some direction as to what they do.
+#
+# https://www.truity.com/page/16-personality-types-myers-briggs
+# https://en.wikipedia.org/wiki/Myers%E2%80%93Briggs_Type_Indicator
+MYERS_BRIGGS = [
+    "[INFP] Healer",
+    "[INTJ] Mastermind",
+    "[INFJ] Councelor",
+    "[INTP] Architect",
+    "[ENFP] Champion",
+    "[ENTJ] Commander",
+    "[ENTP] Visionary",
+    "[ENFJ] Teacher",
+    "[ISFJ] Protector",
+    "[ISFP] Composer",
+    "[ISTJ] Inspector",
+    "[ISTP] Carftperson",
+    "[ESFJ] Provider",
+    "[ESFP] Performer",
+    "[ESTJ] Supervisor",
+    "[ESTP] Dynamo",
+]
+
 
 class NPC:
     """Main NPC class."""
 
-    def __init__(self) -> None:
-        """Initialise the class."""
-        self._jobs = RandomChoice(PROFESSIONS)
+    def __init__(self, what: str = "fantasy") -> None:
+        """Initialise the class.
+
+        The option `what` is special. It defines which files are used for the
+        random values to chose form. The patter is the same for all.
+
+        """
+        rlog.debug(f"Creating {what} NPC")
+        self._data_dir = Path(Path(__file__).resolve().parent, "data")
+        self._jobs = RandomChoice(
+            source=Path(self._data_dir, f"{what}-professions.txt")
+        )
+        self.name = "Random"
+
+        self.personality = RandomChoice(source=MYERS_BRIGGS).get_value()
         self.skill_primary = Skill(self._jobs.get_value(), get_skill_value())
         self.skill_secondary = Skill(self._jobs.get_value(), get_skill_value())
         self.skill_hobby = Skill(self._jobs.get_value(), get_skill_value())
-        rlog.debug("Create NPC")
 
     def __repr__(self) -> str:
         """Pretty print for class.
@@ -29,6 +66,8 @@ class NPC:
         This is the simple console printing. Nothing fancy.
         """
         skills = (
+            f"Name: {self.name}\n"
+            f"Personality: {self.personality}\n"
             f"Skills:\n"
             f"   Primary:   {self.skill_primary}\n"
             f"   Secondary: {self.skill_secondary}\n"
@@ -39,16 +78,30 @@ class NPC:
 
 
 class RandomChoice:
-    """Models a list of things and can get a random item from it."""
+    """Models a list of things and can get a random item from it.
 
-    def __init__(self, lst: list[str] | None = None) -> None:
-        """Initialise class."""
-        if lst is None:
-            # This should load from file or something.
-            lst = ["BAD"]
-        self._list = lst
-        rlog.debug("Choice created")
+    All items in the list have the same weight. A cheap hack is to add the
+    same option multiple times to get a higher probability of being chosen.
+    """
+
+    def __init__(self, source: list[str] | Path) -> None:
+        """Initialise class.
+
+        The `source` can be a file (aka Path) or a list of strings.
+        """
+        self._lst = []
+        if type(source) is PosixPath:
+            src = cast(Path, source)
+            if src.is_file():
+                with Path.open(source) as f:
+                    self._lst = f.read().splitlines()
+            else:
+                rlog.warning(f"There is no such file: {source}")
+                self._lst = ["no choice"]
+            rlog.debug(f"Choice from {source} created")
+        else:
+            self._lst = cast(list[str], source)
 
     def get_value(self) -> str:
         """Return a random value from the list."""
-        return choice(self._list)
+        return choice(self._lst)
---
+++ tests/test_npc.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""NPC class tests."""
+from pathlib import Path
+from typing import Any
+
+import pytest
+
+from pynpc.npc import NPC, RandomChoice
+
+
+@pytest.fixture(autouse=True, scope="package")
+def random() -> Any:
+    return NPC()
+
+
+@pytest.mark.parametrize(
+    ("attr", "expected"),
+    [
+        ("name", True),
+        ("personality", True),
+        ("skill_primary", True),
+        ("skill_secondary", True),
+        ("skill_hobby", True),
+        ("ook", False),
+    ],
+)
+def test_variabe_existss(random, attr, expected) -> None:
+    assert hasattr(random, attr) is expected
+
+
+@pytest.mark.parametrize(
+    ("item"),
+    [
+        ("Name"),
+        ("Personality"),
+        ("Primary"),
+        ("Secondary"),
+    ],
+)
+def test_print(random, item) -> None:
+    assert item in str(random) is not None
+
+
+def test_random_choice_file_not_found() -> None:
+    sut = RandomChoice(Path("/The/Thing/That/Should/Not/Be/name.txt"))
+    assert sut._lst == ["no choice"]
+    assert sut.get_value() == "no choice"
